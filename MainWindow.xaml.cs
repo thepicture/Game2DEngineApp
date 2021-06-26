63836b12194d86a077c1488b084006b9a0140fb6
Student46 <fuzzyeastern@protonmail.com>
2021-06-26 13:20:29 +0500

    Add wall class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Game2DEngineApp
 {
@@ -36,7 +25,13 @@ namespace Game2DEngineApp
             throw new NotImplementedException();
         }
     }
-    class Obstacle : GameObject
+    class Wall : GameObject
+    {
+        public Wall(double x, double y, BitmapImage sprite) : base(x, y, sprite)
+        {
+        }
+    }
+    class Obstacle : Wall
     {
         public Obstacle(double x, double y, BitmapImage sprite) : base(x, y, sprite)
         {
