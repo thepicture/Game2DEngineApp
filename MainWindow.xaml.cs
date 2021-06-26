ac040c26f194955bc2da43a966da34d97062089f
Student46 <fuzzyeastern@protonmail.com>
2021-06-26 13:05:55 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game2DEngineApp
+{
+    class GameObject
+    {
+        private double x;
+        private double y;
+        private BitmapImage sprite;
+
+        public GameObject(double x, double y, BitmapImage sprite)
+        {
+            this.x = x;
+            this.y = y;
+            this.sprite = sprite;
+        }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
